43e882dd48a56860adc5edb1412d4fbb0483c566
Roman Zipp <ich@ich.wtf>
2019-05-30 01:58:46 +0200

    Replace command logic with service

--- src/Console/Commands/DiffEnvFiles.php
+++ src/Console/Commands/DiffEnvFiles.php
@@ -3,11 +3,10 @@
 namespace romanzipp\EnvDiff\Console\Commands;
 
 use Illuminate\Console\Command;
+use romanzipp\EnvDiff\Services\DiffService;
 
 class DiffEnvFiles extends Command
 {
-    const REGEX = '/^([A-Z0-9_]+)/';
-
     /**
      * The name and signature of the console command.
      *
@@ -39,103 +38,13 @@ class DiffEnvFiles extends Command
      */
     public function handle()
     {
-        $exampleData = $this->getEnvContents('.env.example');
+        $files = array_merge(['.env'], config('env-diff.additional_files'));
 
-        $comparingFiles = array_merge(config('env-diff.additional_files'), [null]);
+        $service = new DiffService;
+        $service->add($files);
 
-        foreach ($comparingFiles as $file) {
+        $table = $service->buildTable();
 
-            $file = null === $file ? '.env' : $file;
-
-            $this->info('');
-            $this->info('Comparing environment file: ' . $file);
-            $this->info('');
-
-            $data = $this->getEnvContents($file);
-
-            if ($data === null) {
-                continue;
-            }
-
-            $this->iterateAndEcho($exampleData, $data, $file);
-
-            if ( ! config('env-diff.reverse_example_check')) {
-                continue;
-            }
-
-            $this->iterateAndEcho($data, $exampleData);
-        }
-    }
-
-    /**
-     * Iterate through env varaibles and show results
-     *
-     * @param  array  $data
-     * @param  array  $comparingData
-     * @param  string $file
-     * @return void
-     */
-    public function iterateAndEcho(array $data, array $comparingData, string $file = '.env.example'): void
-    {
-        foreach ($data as $line) {
-
-            if ( ! preg_match(self::REGEX, $line, $match)) {
-                continue;
-            }
-
-            $variable = $match[1];
-
-            if ( ! $this->variableExists($variable, $comparingData)) {
-
-                $dots = str_repeat('.', 20 - strlen($file));
-
-                $this->warn('Missing variable in ' . $file . ' ' . $dots . ': ' . $variable);
-            }
-        }
-    }
-
-    /**
-     * Get splitted contents of environment files
-     *
-     * @param  string  $file Env file name
-     * @return array
-     */
-    protected function getEnvContents(string $file):  ? array
-    {
-        $content = @file_get_contents(base_path() . '/' . $file);
-
-        if ($content === false) {
-            $this->error('Environment file not found: ' . $file);
-
-            return null;
-        }
-
-        $array = explode("\n", $content);
-
-        $array = array_filter($array);
-
-        return $array;
-    }
-
-    /**
-     * Determine if a variable exists
-     *
-     * @param  string    $variable Variable name
-     * @param  array     $envData  Environment data
-     * @return boolean
-     */
-    protected function variableExists(string $variable, array $envData) : bool
-    {
-        foreach ($envData as $envLine) {
-
-            if (preg_match(self::REGEX, $envLine, $match)) {
-
-                if ($match[1] == $variable) {
-                    return true;
-                }
-            }
-        }
-
-        return false;
+        $table->display();
     }
 }
